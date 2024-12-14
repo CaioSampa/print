b74a7bfeabf96fabc59bc897ac0f7fdb6208185a
Caio S <caiosampaio604@gmail.com>
2024-12-14 15:52:27 -0300

    [fix] alphabet

--- ft_print_alphabet.c
+++ ft_print_alphabet.c
@@ -10,12 +10,11 @@ int main (void) {
 }
 
 void ft_print_alphabt(void){
-    char alph[] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
-    int index = 0;
+    char letter = 'a';
 
-    while(index < (sizeof(alph) / sizeof(alph[0]))){
-        write(1, &alph[index], 1);
-        index++;
+    while(letter != '{'){
+        write(1, &letter, 1);
+        letter++;
     }
 
 }
